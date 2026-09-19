49757c9c37166d66b9dbdc1319fc7bbea5965d8a
ssumiran <ssumiran@yahoo.com>
2026-09-19 18:34:08 -0400

    Initial Monolithic University SpringBoot

---
+++ isuniversity/src/main/java/u/university/IsuniversityApplication.java
@@ -0,0 +1,13 @@
+package u.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsuniversityApplication.class, args);
+	}
+
+}
---
+++ isuniversity/src/test/java/u/university/IsuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package u.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
