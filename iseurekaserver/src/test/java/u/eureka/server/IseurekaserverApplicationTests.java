06b8ebccd43cdbe4026c70d5110a7717ce30225a
ssumiran <ssumiran@yahoo.com>
2026-09-21 11:09:09 -0400

    Init Eureka Server

---
+++ iseurekaserver/src/test/java/u/eureka/server/IseurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package u.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IseurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
