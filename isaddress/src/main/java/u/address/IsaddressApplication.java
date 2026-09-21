f244a6a4a781dd37c7ac0f732c7cddf8efa73b3a
ssumiran <ssumiran@yahoo.com>
2026-09-21 11:28:40 -0400

    Enable Eureka Discovery Client on each Microservice Address and Student

--- isaddress/src/main/java/u/address/IsaddressApplication.java
+++ isaddress/src/main/java/u/address/IsaddressApplication.java
@@ -2,11 +2,13 @@ package u.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@ComponentScan({"u.address.controller", "u.address.service"})
 //@EntityScan("u.address.entity")
 //@EnableJpaRepositories("com.infybuzz.repository")
+@EnableDiscoveryClient
 public class IsaddressApplication {
 
 	public static void main(String[] args) {
--- isstudent/src/main/java/u/student/IsstudentApplication.java
+++ isstudent/src/main/java/u/student/IsstudentApplication.java
@@ -3,6 +3,7 @@ package u.student;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 //@EntityScan("u.student.entity")
 //@EnableJpaRepositories("u.student.repository")
 @EnableFeignClients("u.student.cloud.feignclient")
+@EnableDiscoveryClient
 public class IsstudentApplication {
 
 	@Value("${address.service.url}")
