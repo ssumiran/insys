1247774dc1742132ccf39aaeb49282bc7f1639e5
ssumiran <ssumiran@yahoo.com>
2026-09-21 11:13:45 -0400

    Enable Eureka server

--- iseurekaserver/src/main/java/u/eureka/server/EurekaserverApplication.java
+++ iseurekaserver/src/main/java/u/eureka/server/EurekaserverApplication.java
@@ -2,8 +2,10 @@ package u.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
